ff46086c94a54152a4adbdc5d42d9175ed6c4558
christinema825 <christinema0825@gmail.com>
2024-01-16 18:02:16 -0800

    initial react commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+// import { ThemeProvider, createTheme } from '@mui/material/styles';
+import FileSystemNavigator from './fileSystemNavigator.js';
+import Button from '@mui/material/Button';
+// const theme = createTheme();
+
+function App() {
+  // return <Button variant="contained">Hello world</Button>;
+  return <FileSystemNavigator/>
+  // return <p>hi</p>
+}
+
+
+export default App;
